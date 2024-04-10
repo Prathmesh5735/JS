d0ca4b22e63d395c2b808b08ae2b9601b5376f81
Prathmesh Chaudhari <140944700+Prathmesh5735@users.noreply.github.com>
2024-04-10 05:45:11 +0000

    Conditional Statements problems

---
+++ Conditional_Statements/02.js
@@ -0,0 +1,9 @@
+// Create a JavaScript program that determines whether a person is eligible to vote based on their age.
+
+let age = 25;
+
+if(age>=18){
+    console.log("You are eligible to vote");
+}else{
+    console.log("You are not eligible");
+}
---
+++ Conditional_Statements/03.js
@@ -0,0 +1,9 @@
+// Write a JavaScript program that determines whether a given character is a vowel or a consonant.
+
+let char="a";
+
+if(char == "a" || char == "e" || char == "i" || char == "o" || char == "u") {
+    console.log("Vowel");
+}else{
+    console.log("Consonant");
+}
